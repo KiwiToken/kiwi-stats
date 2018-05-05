590882f7d1840ef6b0a177caaf3479ca4c547c71
amexperts <am.experts.online@gmail.com>
2018-05-05 17:08:20 +0200

    hack to reflect more accurate hash power when difficulty is below 1000

--- js/main.js
+++ js/main.js
@@ -309,7 +309,7 @@ function updateStatsThatHaveDependencies(stats) {
 
   hashrate = difficulty * 2**22 / 120
   if(difficulty < 1000) {
-      hashrate *= 1000;
+      hashrate 1000 * 2**22 / 120;
   }
   /* use current reward rate in hashrate calculation */
   hashrate *= (2 / minutes_per_reward)
