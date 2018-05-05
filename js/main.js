56d9b2c8fd53ca20bd2fe3c0f8f94f14b08bb423
amexperts <am.experts.online@gmail.com>
2018-05-05 11:41:14 +0200

    working on displaying token info

--- js/main.js
+++ js/main.js
@@ -79,18 +79,23 @@ eth.coinbase().then((result) => {
    var network = "unknown network";
   switch (netId) {
     case "1":
+      network = "Main Ethereum Network";
       console.log('This is mainnet')
       break
     case "2":
+      network = "Deprecated Morden Network";
       console.log('This is the deprecated Morden test network.')
       break
     case "3":
+      network = "Ropsten Test Network";
       console.log('This is the ropsten test network.')
       break
     case "4":
+      network = "Rinkeby Test Network";
       console.log('This is the Rinkeby test network.')
       break
     case "42":
+      network = "Kovan Test Network";
       console.log('This is the Kovan test network.')
       break
     default:
