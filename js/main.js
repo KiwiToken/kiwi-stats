17d718350d39e8a9812fa9866254c5c6106a0ada
amexperts <am.experts.online@gmail.com>
2018-05-09 14:12:57 +0200

    showing gas used

--- js/main.js
+++ js/main.js
@@ -566,12 +566,12 @@ function updateAllMinerInfo(eth, stats, hours_into_past){
           web3.eth.getTransaction(tx_hash, function(e, r){
             if(!e) {
               return parseInt(web3.fromWei(r.gasPrice, 'Gwei'),10);
-            }
+            };
           });
 
       return '~~';
     }
-
+  }
     /* fill in block info */
     var dt = new Date();
     var innerhtml_buffer = '<tr><th>Time (Approx)</th><th>Eth Block #</th>'
